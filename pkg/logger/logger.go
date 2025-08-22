ce4fecc69fe9d275f0cb2d0c4ef0cbcbc69c9149
ArtDark <balusoff@gmail.com>
2025-08-22 16:44:32 +0300

    add slog as logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+	"publicator/config"
+)
+
+type Service struct {
+	Info  *slog.Logger
+	Error *slog.Logger
+}
+
+func NewService(lc *config.LogConfig) *Service {
+	if lc.Type == "json" {
+		return &Service{
+			Info:  slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.Level(lc.Level)})),
+			Error: slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.Level(lc.Level)})),
+		}
+	}
+
+	return &Service{
+		Info:  slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.Level(lc.Level)})),
+		Error: slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.Level(lc.Level)})),
+	}
+
+}
